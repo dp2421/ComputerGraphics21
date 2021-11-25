7003b028713d8a7c7a859de94d47a6181d963110
tngus0707 <rldrkd07@naver.com>
2021-11-26 00:49:26 +0900

    새로 폴더 추가
    변동 사항 없어요

--- CGproject_SH/CGproject_SH/CGproject_SH.cpp
+++ CGproject_SH/CGproject_SH/CGproject_SH.cpp
@@ -5,22 +5,26 @@
 #include <gl/freeglut_ext.h>
 #include <gl/glm/glm.hpp>
 #include <gl/glm/ext.hpp>
-#include <gl/glm/gtc/matrix_transform.hpp>
+#include <gl/glm/gtc/matrix_transform.hpp> //예나
 //#include <glm/glm/glm.hpp>
 //#include <glm/glm/ext.hpp>
-//#include <glm/glm/gtc/matrix_transform.hpp>
+//#include <glm/glm/gtc/matrix_transform.hpp> //수현
+
 
 ////////너랑 나랑 gl경로가 달라서 서로 상대방 거 주석처리하고 사용하는 걸로 하자!/////////
 
+
+////////////////////미래의 상대방에게 전하는 메시지////////////////////
 //아항 알게써!!
+//하몰겟ㄷㅏ이걸어쩌냐.............initbuffer를 드로우씬에 호출하면,,안되는데,,메모리 내 일반 과제보다 100배정도 올라가더라 ㅠ
 
 using namespace std;
 
 GLvoid drawScene(GLvoid);
 GLvoid Reshape(int, int);
 GLvoid CubeInitBuffer();
-GLvoid GroudInitBuffer();
-GLvoid InitShader();
+GLvoid GroundInitBuffer();
+//GLvoid InitShader();
 
 void make_vertexShaders();
 void make_fragmentShaders();
@@ -38,7 +42,7 @@ GLchar errorLog[512];
 
 GLchar* filetobuf(const char* file);
 
-GLuint vao, vbo[2];
+GLuint vao[2], vbo[2], ebo;
 
 //육면체 위치 변수
 GLfloat CubePosX;
@@ -53,166 +57,58 @@ GLfloat CamPosZ = 50.0f;
 GLfloat CamDirX = CubePosX;
 GLfloat CamDirZ = CubePosZ - 7.0f;
 
+
 //wasd check
 bool checkW = false;
 bool checkA = false;
 bool checkS = false;
 bool checkD = false;
 
-GLfloat ground[][9] = {
-	{   -1.0f, 0.0f, -1.0f,
-		-1.0f, 0.0f, 1.0f,
-		1.0f, 0.0f, -1.0f   },
-
-	{   1.0f, 0.0f, -1.0f,
-		-1.0f, 0.0f, 1.0f,
-		1.0f, 0.0f, 1.0f    }
+GLfloat ground[][3] = {
+	-1, 0.0, -1,
+	1, 0.0, -1,
+	-1, 0.0, 1,
+	1, 0.0, 1,
 };
 
-GLfloat ground_color[] = { //Misty Rose
+GLfloat ground_color[][3] = {
 	0.690196, 0.768627, 0.870588,
-	0.690196, 0.768627, 0.870588,
-	0.690196, 0.768627, 0.870588,
-
-	0.690196, 0.768627, 0.870588,
-	0.690196, 0.768627, 0.870588,
-	0.690196, 0.768627, 0.870588,
-
-	0.690196, 0.768627, 0.870588,
-	0.690196, 0.768627, 0.870588,
-	0.690196, 0.768627, 0.870588,
-
-	0.690196, 0.768627, 0.870588,
-	0.690196, 0.768627, 0.870588,
-	0.690196, 0.768627, 0.870588,
-
-	0.690196, 0.768627, 0.870588,
-	0.690196, 0.768627, 0.870588,
-	0.690196, 0.768627, 0.870588,
-
-	0.690196, 0.768627, 0.870588,
-	0.690196, 0.768627, 0.870588,
-	0.690196, 0.768627, 0.870588,
-
-	0.690196, 0.768627, 0.870588,
-	0.690196, 0.768627, 0.870588,
-	0.690196, 0.768627, 0.870588,
-
-	0.690196, 0.768627, 0.870588,
-	0.690196, 0.768627, 0.870588,
-	0.690196, 0.768627, 0.870588,
-
-	0.690196, 0.768627, 0.870588,
-	0.690196, 0.768627, 0.870588,
-	0.690196, 0.768627, 0.870588,
-
-	0.690196, 0.768627, 0.870588,
-	0.690196, 0.768627, 0.870588,
-	0.690196, 0.768627, 0.870588,
-
-	0.690196, 0.768627, 0.870588,
-	0.690196, 0.768627, 0.870588,
-	0.690196, 0.768627, 0.870588,
-
 	0.690196, 0.768627, 0.870588,
 	0.690196, 0.768627, 0.870588,
 	0.690196, 0.768627, 0.870588,
 };
 
-GLfloat cube[][9] = {
+GLuint ground_element[] = { 2, 0, 1, 2, 1, 3, };
 
-	//윗면
-	{   -0.1, 0.1, -0.1,
-		-0.1, 0.1, 0.1,
-		0.1, 0.1, -0.1   },
-	{   0.1, 0.1, -0.1,
-		-0.1, 0.1, 0.1,
-		0.1, 0.1, 0.1   },
-		//아랫면
-		{   -0.1, -0.1, 0.1,
-			-0.1, -0.1, -0.1,
-			0.1, -0.1, 0.1   },
-		{   0.1, -0.1, 0.1,
-			-0.1, -0.1, -0.1,
-			0.1, -0.1, -0.1   },
-			//앞면
-			{   -0.1, 0.1, 0.1,
-				-0.1, -0.1, 0.1,
-				0.1, 0.1, 0.1   },
-			{   0.1, 0.1, 0.1,
-				-0.1, -0.1, 0.1,
-				0.1, -0.1, 0.1   },
-				//오른쪽면
-				{   0.1, 0.1, 0.1,
-					0.1, -0.1, 0.1,
-					0.1, 0.1, -0.1   },
-				{   0.1, 0.1, -0.1,
-					0.1, -0.1, 0.1,
-					0.1, -0.1, -0.1   },
-					//뒷면
-					{   0.1, 0.1, -0.1,
-						0.1, -0.1, -0.1,
-						-0.1, 0.1, -0.1   },
-					{   -0.1, 0.1, -0.1,
-						0.1, -0.1, -0.1,
-						-0.1, -0.1, -0.1   },
-						//왼쪽면
-						{   -0.1, 0.1, -0.1,
-							-0.1, -0.1, -0.1,
-							-0.1, 0.1, 0.1   },
-						{   -0.1, 0.1, 0.1,
-							-0.1, -0.1, -0.1,
-							-0.1, -0.1, 0.1   }
+GLfloat cube[][3] = {
+	-0.1, -0.1, -0.1,
+	0.1, -0.1, -0.1,
+	-0.1, 0.1, -0.1,
+	0.1, 0.1, -0.1,
+	-0.1, -0.1, 0.1,
+	0.1, -0.1, 0.1,
+	-0.1, 0.1, 0.1,
+	0.1, 0.1, 0.1,
 };
 
-GLfloat cube_color[] = { //Light Pink
+GLfloat cube_color[][3] = {
 	1, 0.713725, 0.756863,
 	1, 0.713725, 0.756863,
 	1, 0.713725, 0.756863,
+	1, 0.713725, 0.756863,
+	1, 0.713725, 0.756863,
+	1, 0.713725, 0.756863,
+	1, 0.713725, 0.756863,
+	1, 0.713725, 0.756863,
+};
 
-	1, 0.713725, 0.756863,
-	1, 0.713725, 0.756863,
-	1, 0.713725, 0.756863,
-
-	1, 0.713725, 0.756863,
-	1, 0.713725, 0.756863,
-	1, 0.713725, 0.756863,
-
-	1, 0.713725, 0.756863,
-	1, 0.713725, 0.756863,
-	1, 0.713725, 0.756863,
-
-	1, 0.713725, 0.756863,
-	1, 0.713725, 0.756863,
-	1, 0.713725, 0.756863,
-
-	1, 0.713725, 0.756863,
-	1, 0.713725, 0.756863,
-	1, 0.713725, 0.756863,
-
-	1, 0.713725, 0.756863,
-	1, 0.713725, 0.756863,
-	1, 0.713725, 0.756863,
-
-	1, 0.713725, 0.756863,
-	1, 0.713725, 0.756863,
-	1, 0.713725, 0.756863,
-
-	1, 0.713725, 0.756863,
-	1, 0.713725, 0.756863,
-	1, 0.713725, 0.756863,
-
-	1, 0.713725, 0.756863,
-	1, 0.713725, 0.756863,
-	1, 0.713725, 0.756863,
-
-	1, 0.713725, 0.756863,
-	1, 0.713725, 0.756863,
-	1, 0.713725, 0.756863,
-
-	1, 0.713725, 0.756863,
-	1, 0.713725, 0.756863,
-	1, 0.713725, 0.756863,
+GLuint cubelement[36] = {
+	2, 0, 1, 2, 1, 3,
+	0, 4, 5, 0, 5, 1,
+	3, 1, 5, 3, 5, 7,
+	7, 5, 4, 7, 4, 6,
+	6, 4, 0, 6, 0, 2,
+	6, 2, 3, 6, 3, 7
 };
 
 GLvoid Keyboard(unsigned char key, int x, int y)
@@ -253,23 +149,29 @@ GLvoid Keyboard(unsigned char key, int x, int y)
 		checkD = true;
 		break;
 		/////////////////////////////카메라가 이동할 수 있는게 짜기 편할 것 같아서 x,y,z로 이동할 수 있게 해놨어!!!///////////////////////
+		///////////카메라 이동시 CameraDirection도 같이 변경해주어야 해서 수정///////////
 	case 'x':
 		CamPosX += 1.0f;
+		CamDirX += 1.f;	//요게 추가한거
 		break;
 	case 'y':
 		CamPosY += 1.0f;
+		//CamDirY += 1.f;
 		break;
 	case 'z':
 		CamPosZ += 1.0f;
+		CamDirZ += 1.f;
 		break;
 	case 'X':
 		CamPosX -= 1.0f;
+		CamDirX -= 1.f;
 		break;
 	case 'Y':
 		CamPosY -= 1.0f;
 		break;
 	case 'Z':
 		CamPosZ -= 1.0f;
+		CamDirX -= 1.f;
 		break;
 	case 'q':
 		exit(0);
@@ -310,31 +212,25 @@ GLvoid Projection()
 GLvoid Ground()
 {
 	//바닥 그리기=========================================================================================================================
-	glm::mat4 Scale = glm::mat4(1.0f);
-	Scale = glm::scale(glm::mat4(1.0f), glm::vec3(10.0f, 2.0f, 50.0f));
+	glm::mat4 Scale = glm::scale(glm::mat4(1.0f), glm::vec3(10.0f, 2.0f, 50.0f));
 
 	unsigned int TransformLocation = glGetUniformLocation(s_program, "modelTransForm"); //--- 버텍스 세이더에서 모델링 변환 위치 가져오기
 	glUniformMatrix4fv(TransformLocation, 1, GL_FALSE, glm::value_ptr(Scale)); //--- modelTransform 변수에 변환 값 적용하기
-	
-	GroudInitBuffer();
-	glDrawArrays(GL_TRIANGLES, 0, 6);
+	glBindVertexArray(vao[0]);
+	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 }
 
 GLvoid Cube()
 {
-	glm::mat4 Trans = glm::mat4(1.0f);
-	Trans = glm::translate(glm::mat4(1.0f), glm::vec3(CubePosX, 0.2f, CubePosZ));
-	glm::mat4 Scale = glm::mat4(1.0f);
-	Scale = glm::scale(glm::mat4(1.0f), glm::vec3(1.0f, 1.0f, 1.0f));
-	glm::mat4 Mat_Cube = glm::mat4(1.0f);
-	Mat_Cube = Scale * Trans;
-	
+	glm::mat4 Trans = glm::translate(glm::mat4(1.0f), glm::vec3(CubePosX, 0.2f, CubePosZ));
+	glm::mat4 Scale = glm::scale(glm::mat4(1.0f), glm::vec3(1.0f, 1.0f, 1.0f));
+	glm::mat4 Mat_Cube = Scale * Trans;
+
 	GLuint TransformLocation = glGetUniformLocation(s_program, "modelTransForm"); //--- 버텍스 세이더에서 모델링 변환 위치 가져오기
 	glUniformMatrix4fv(TransformLocation, 1, GL_FALSE, glm::value_ptr(Mat_Cube)); //--- modelTransform 변수에 변환 값 적용하기
-
-	CubeInitBuffer();
+	glBindVertexArray(vao[1]);
 	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-	glDrawArrays(GL_TRIANGLES, 0, 36);
+	glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
 }
 
 void drawScene() //--- glutDisplayFunc()함수로 등록한 그리기 콜백 함수
@@ -347,7 +243,7 @@ void drawScene() //--- glutDisplayFunc()함수로 등록한 그리기 콜백 함수
 	Projection(); //투영
 	Ground(); //바닥 그리기
 	Cube(); //객체 그리기
-
+	//glFrontFace(GL_CCW);
 	glutPostRedisplay();
 	glutSwapBuffers();
 }
@@ -365,10 +261,9 @@ void main(int argc, char** argv) //--- 윈도우 출력하고 콜백함수 설정
 	glewExperimental = GL_TRUE;
 	glewInit();
 	make_shaderProgram();
+	CubeInitBuffer();
+	GroundInitBuffer();
 	glEnable(GL_DEPTH_TEST);
-	glGenVertexArrays(1, &vao); //--- VAO 를 지정하고 할당하기
-	glBindVertexArray(vao); //--- VAO를 바인드하기
-	glGenBuffers(2, vbo); //--- 2개의 VBO를 지정하고 할당하기
 
 	glutDisplayFunc(drawScene);
 	glutReshapeFunc(Reshape);
@@ -378,34 +273,56 @@ void main(int argc, char** argv) //--- 윈도우 출력하고 콜백함수 설정
 	glutMainLoop();
 }
 
-GLvoid CubeInitBuffer()
+GLvoid GroundInitBuffer()
 {
-	glGenVertexArrays(1, &vao); //--- VAO 를 지정하고 할당하기
-	glBindVertexArray(vao); //--- VAO를 바인드하기
-	glGenBuffers(2, vbo); //--- 2개의 VBO를 지정하고 할당하기
+	glGenVertexArrays(1, &vao[0]);
+	glBindVertexArray(vao[0]);
 
-	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(cube), cube, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-	glEnableVertexAttribArray(0);
-
-	glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(cube_color), cube_color, GL_STATIC_DRAW);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, 0);
-	glEnableVertexAttribArray(1);
-}
-
-GLvoid GroudInitBuffer()
-{
+	glGenBuffers(1, &vbo[0]);
 	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(ground), ground, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), 0);
 	glEnableVertexAttribArray(0);
 
+	glGenBuffers(1, &vbo[1]);
 	glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
+
 	glBufferData(GL_ARRAY_BUFFER, sizeof(ground_color), ground_color, GL_STATIC_DRAW);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), 0);	//첫 번째 인자: 인덱스
 	glEnableVertexAttribArray(1);
+
+	glGenBuffers(1, &ebo);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo); // GL_ELEMENT_ARRAY_BUFFER 버퍼 유형으로 바인딩
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(ground_element), ground_element, GL_STATIC_DRAW);
+	glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), 0);
+	glEnableVertexAttribArray(2);	//vao에 vbo를 묶어줌
+}
+
+GLvoid CubeInitBuffer()
+{
+	glGenVertexArrays(1, &vao[1]);
+	glBindVertexArray(vao[1]);
+
+	glGenBuffers(1, &vbo[0]);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(cube), cube, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), 0);
+	glEnableVertexAttribArray(0);
+
+	glGenBuffers(1, &vbo[1]);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
+
+	glBufferData(GL_ARRAY_BUFFER, sizeof(cube_color), cube_color, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), 0);	//첫 번째 인자: 인덱스
+	glEnableVertexAttribArray(1);
+
+	glGenBuffers(1, &ebo);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo); // GL_ELEMENT_ARRAY_BUFFER 버퍼 유형으로 바인딩
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(cubelement), cubelement, GL_STATIC_DRAW);
+	glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), 0);
+	glEnableVertexAttribArray(2);	//vao에 vbo를 묶어줌
 }
 
 //GLvoid InitShader()
