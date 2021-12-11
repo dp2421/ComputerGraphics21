151d208b83ca5f747aed12e43a836a5dd6200aa9
tngus0707 <rldrkd07@naver.com>
2021-12-11 20:20:03 +0900

    확인 부탁드려용

--- CGproject_SH/CGproject_SH/CGproject_SH.cpp
+++ CGproject_SH/CGproject_SH/CGproject_SH.cpp
@@ -1,4 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 #include <iostream>
 #include <random>
 #include <gl/glew.h>
@@ -14,7 +16,6 @@
 
 ////////너랑 나랑 gl경로가 달라서 서로 상대방 거 주석처리하고 사용하는 걸로 하자!/////////
 
-
 ////////////////////미래의 상대방에게 전하는 메시지////////////////////
 
 //쉐이더 수정하면서 컬러값을 좌표가 아니라 밑에 드로우에서 glUniform3f(Color_1, 0.690196, 0.768627, 0.870588); 이렇게 받아오는걸로 수정
@@ -24,13 +25,15 @@
 //그리고 스테이지 1에서 끝까지 가면 플레이어가 뒤돌고 스테이지 2 바로 시작!
 
 using namespace std;
+unsigned int texture[1];
 
 GLvoid drawScene(GLvoid);
 GLvoid Reshape(int, int);
 GLvoid CubeInitBuffer();
 GLvoid GroundInitBuffer();
 GLvoid PlayerInitBuffer();
-//GLvoid InitShader();
+GLvoid CarInitBuffer();
+GLvoid InitTexture();
 
 void make_vertexShaders();
 void make_fragmentShaders();
@@ -49,7 +52,7 @@ GLchar errorLog[512];
 
 GLchar* filetobuf(const char* file);
 
-GLuint vao[3], vbo[2], ebo;
+GLuint vao[4], vbo[3], ebo;
 
 //육면체 위치 변수
 GLfloat CubePosX = 0.0f;
@@ -91,7 +94,8 @@ int map2[30] = { 0,0,0,1,1,1,0,0,1,1,1,0,0,0,1,1,0,1,0,1,1,1,0,0,1,1,1,0,0,0 };
 //obj 관련 변수 
 int loadObj(const char* filename);
 int loadObj_normalize_center(const char* filename);	//불러오는 함수들 (밑에있음)
-float* sphere_object;
+int loadObj_normalize_center1(const char* filename);
+
 int num_Triangle;
 const int num_vertices = 3;
 const int num_triangles = 1;
@@ -109,6 +113,21 @@ float maxX = 0.0, maxY = 0.0, maxZ = 0.0;
 float scaleAll;
 
 
+int num_Triangle1;
+const int num_vertices1 = 3;
+const int num_triangles1 = 1;
+std::vector< unsigned int > vertexIndices1, uvIndices1, normalIndices1;
+std::vector< glm::vec3 > temp_vertices1;
+std::vector< glm::vec2 > temp_uvs1;
+std::vector< glm::vec3 > temp_normals1;
+std::vector< glm::vec3 > outvertex1, outnormal1;
+std::vector< glm::vec2 > outuv1;
+float sumX1 = 0.0, sumY1 = 0.0, sumZ1 = 0.0;
+float aveX1, aveY1, aveZ1;
+float scaleX1, scaleY1, scaleZ1;
+float minX1 = 0.0, minY1 = 0.0, minZ1 = 0.0;
+float maxX1 = 0.0, maxY1 = 0.0, maxZ1 = 0.0;
+float scaleAll1;
 struct BB {
 	float minx;
 	float minz;
@@ -170,6 +189,14 @@ GLfloat cube[][3] = {
 	0.5, 0.5, 0.5,
 };
 
+GLfloat ground_Texture[][6] = {
+	{0.0, 1.0,
+	0.0, 0.0,
+	1.0, 1.0},
+	{1.0, 1.0,
+	0.0, 0.0,
+	1.0, 0.0},
+};
 
 GLuint cubelement[36] = {
 	2, 0, 1, 2, 1, 3,
@@ -355,7 +382,7 @@ GLvoid Car1()
 		CarPosZ = 30 - 2 * i;
 		if (map1[i] == 1)
 		{
-			glm::mat4 Scale = glm::scale(glm::mat4(1.0f), glm::vec3(1.f, 0.7f, 1.f));
+			glm::mat4 Scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f, 0.1f, 0.1f));
 			glm::mat4 BottomScale = glm::scale(glm::mat4(1.f), glm::vec3(2.f, 1.f, 1.f));
 			glm::mat4 UpperTrans = glm::translate(glm::mat4(1.f), glm::vec3(0, 0.7, 0));
 			glm::mat4 Mat_Car = glm::mat4(1.f);
@@ -378,17 +405,14 @@ GLvoid Car1()
 			default:
 				Mat_Car = Carmoving * Trans;
 			}
-			Trans_Car = Mat_Car * UpperTrans * Scale;
+			//Trans_Car = Mat_Car * UpperTrans * Scale;
+			Trans_Car = Mat_Car;
 			GLuint TransformLocation = glGetUniformLocation(s_program, "modelTransForm"); //--- 버텍스 세이더에서 모델링 변환 위치 가져오기
 			glUniformMatrix4fv(TransformLocation, 1, GL_FALSE, glm::value_ptr(Trans_Car)); //--- modelTransform 변수에 변환 값 적용하기
-			glBindVertexArray(vao[1]);
+			glBindVertexArray(vao[3]);
 			glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-			glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+			glDrawArrays(GL_TRIANGLES, 0, num_Triangle1);
 
-			Trans_Car = Mat_Car * BottomScale;
-			glUniformMatrix4fv(TransformLocation, 1, GL_FALSE, glm::value_ptr(Trans_Car)); //--- modelTransform 변수에 변환 값 적용하기
-			glBindVertexArray(vao[1]);
-			glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
 			if (checkCrash1 == false)
 			{
 				//cout << cnt[0] << endl;
@@ -451,17 +475,17 @@ GLvoid Car2()
 			default:
 				Mat_Car = Carmoving * 1.5f * Trans;
 			}
-			Trans_Car = Mat_Car * UpperTrans * Scale;
+			Trans_Car = Mat_Car;
 			GLuint TransformLocation = glGetUniformLocation(s_program, "modelTransForm"); //--- 버텍스 세이더에서 모델링 변환 위치 가져오기
 			glUniformMatrix4fv(TransformLocation, 1, GL_FALSE, glm::value_ptr(Trans_Car)); //--- modelTransform 변수에 변환 값 적용하기
-			glBindVertexArray(vao[1]);
+			glBindVertexArray(vao[3]);
 			glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-			glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+			glDrawArrays(GL_TRIANGLES, 0, num_Triangle1);
 
-			Trans_Car = Mat_Car * BottomScale;
-			glUniformMatrix4fv(TransformLocation, 1, GL_FALSE, glm::value_ptr(Trans_Car)); //--- modelTransform 변수에 변환 값 적용하기
-			glBindVertexArray(vao[1]);
-			glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+			//Trans_Car = Mat_Car * BottomScale;
+			//glUniformMatrix4fv(TransformLocation, 1, GL_FALSE, glm::value_ptr(Trans_Car)); //--- modelTransform 변수에 변환 값 적용하기
+			//glBindVertexArray(vao[1]);
+			//glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
 			if (checkCrash2 == false)
 			{
 				if (Collide(getbb_player(CubePosX, CubePosZ), getbb_car(cnt[0], CarPosZ)) == true) //차에 큐브가 갖다 들이박기, 지나가는 차에 치이기 둘 다 충돌 처리
@@ -485,6 +509,70 @@ GLvoid Light()
 	glUniform3f(lightColorLocation, 1.0, 1.0, 1.0);
 	glUniform3f(lightPosLocation, CubePosX, 20, CubePosZ);
 }
+
+GLvoid InitTexture()
+{
+	BITMAP* bmp;
+
+	int width, height, nrChannels;
+
+	glGenTextures(1, &texture[0]); //--- 텍스처 생성
+
+	glBindTexture(GL_TEXTURE_2D, texture[0]); //--- 텍스처 바인딩
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT); //--- 현재 바인딩된 텍스처의 파라미터 설정하기
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	stbi_set_flip_vertically_on_load(true);
+
+	unsigned char* data = stbi_load("stage1.png", &width, &height, &nrChannels, 0);
+	glTexImage2D(GL_TEXTURE_2D, 0, 3, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data); //---텍스처 이미지 정의
+
+	glUseProgram(shaderID);
+	int tLocation = glGetUniformLocation(s_program, "outTexture"); //--- outTexture 유니폼 샘플러의 위치를 가져옴
+	glUniform1i(tLocation, 0); //--- 샘플러를 0번 유닛으로 설정
+	stbi_image_free(data);
+}
+
+GLvoid GroundInitBuffer()
+{
+	glGenVertexArrays(1, &vao[0]);
+	glBindVertexArray(vao[0]);
+
+	glGenBuffers(1, &vbo[0]);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(ground), ground, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), 0);
+	glEnableVertexAttribArray(0);
+
+	glGenBuffers(1, &vbo[1]);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(ground), ground, GL_STATIC_DRAW);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), 0);	//첫 번째 인자: 인덱스
+	glEnableVertexAttribArray(1);
+
+	glGenBuffers(1, &vbo[2]);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[2]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(ground_Texture), ground_Texture, GL_STATIC_DRAW);
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
+	glEnableVertexAttribArray(2);
+
+	glGenBuffers(1, &ebo);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo); // GL_ELEMENT_ARRAY_BUFFER 버퍼 유형으로 바인딩
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(ground_element), ground_element, GL_STATIC_DRAW);
+	glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), 0);
+	//glEnableVertexAttribArray(2);	//vao에 vbo를 묶어줌
+}
+
+GLvoid GroundTexture()
+{
+	Ground();
+	glBindVertexArray(vao[0]); //--- 첫 번째 폴리곤
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, texture[0]); //--- texture[0]을 사용하여 폴리곤을 그린다.
+	glDrawArrays(GL_TRIANGLES, 0, 6);
+}
+
 void drawScene() //--- glutDisplayFunc()함수로 등록한 그리기 콜백 함수
 {
 	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
@@ -531,38 +619,17 @@ void main(int argc, char** argv) //--- 윈도우 출력하고 콜백함수 설정
 	CubeInitBuffer();
 	GroundInitBuffer();
 	PlayerInitBuffer();
+	CarInitBuffer();
 	Color_1 = glGetUniformLocation(s_program, "in_Color");
 	glEnable(GL_DEPTH_TEST);
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, texture[0]);
 
 	glutDisplayFunc(drawScene);
 	glutReshapeFunc(Reshape);
 	glutKeyboardFunc(Keyboard);
 	glutTimerFunc(10, TimeFunction, 1);
 	glutMainLoop();
-}
-
-GLvoid GroundInitBuffer()
-{
-	glGenVertexArrays(1, &vao[0]);
-	glBindVertexArray(vao[0]);
-
-	glGenBuffers(1, &vbo[0]);
-	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(ground), ground, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), 0);
-	glEnableVertexAttribArray(0);
-
-	glGenBuffers(1, &vbo[1]);
-	glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(ground), ground, GL_STATIC_DRAW);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), 0);	//첫 번째 인자: 인덱스
-	glEnableVertexAttribArray(1);
-
-	glGenBuffers(1, &ebo);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo); // GL_ELEMENT_ARRAY_BUFFER 버퍼 유형으로 바인딩
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(ground_element), ground_element, GL_STATIC_DRAW);
-	glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), 0);
-	//glEnableVertexAttribArray(2);	//vao에 vbo를 묶어줌
 }
 
 GLvoid PlayerInitBuffer()
@@ -610,7 +677,27 @@ GLvoid CubeInitBuffer()
 	glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), 0);
 	glEnableVertexAttribArray(2);	//vao에 vbo를 묶어줌
 }
+GLvoid CarInitBuffer()
+{
+	num_Triangle1 = loadObj_normalize_center1("car.obj");
+	//// 5.1. VAO 객체 생성 및 바인딩
+	glGenVertexArrays(2, &vao[3]);
+	glGenBuffers(2, &vbo[0]);
+	glGenBuffers(2, &vbo[1]);
 
+	glBindVertexArray(vao[3]);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+	glBufferData(GL_ARRAY_BUFFER, outvertex1.size() * sizeof(glm::vec3), &outvertex1[0], GL_STATIC_DRAW);
+	GLint pAttribute = glGetAttribLocation(s_program, "vPos");
+	glVertexAttribPointer(pAttribute, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), 0);
+	glEnableVertexAttribArray(pAttribute);
+
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
+	glBufferData(GL_ARRAY_BUFFER, outnormal1.size() * sizeof(glm::vec3), &outnormal1[0], GL_STATIC_DRAW);
+	GLint nAttribute = glGetAttribLocation(s_program, "vNormal");
+	glVertexAttribPointer(nAttribute, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), 0);
+	glEnableVertexAttribArray(nAttribute);
+}
 GLvoid Reshape(int w, int h) //--- 콜백 함수: 다시 그리기 콜백 함수
 {
 	glViewport(0, 0, w, h);
@@ -618,7 +705,7 @@ GLvoid Reshape(int w, int h) //--- 콜백 함수: 다시 그리기 콜백 함수
 
 void make_vertexShaders()
 {
-	vertexsource = filetobuf("vertex.glsl");
+	vertexsource = filetobuf("lightvertex.glsl");
 
 	//--- 버텍스 세이더 객체 만들기
 	vertexShader = glCreateShader(GL_VERTEX_SHADER);
@@ -643,7 +730,7 @@ void make_vertexShaders()
 
 void make_fragmentShaders()
 {
-	fragmentsource = filetobuf("fragment.glsl");
+	fragmentsource = filetobuf("mirrorfragment.glsl");
 	//--- 프래그먼트 세이더 객체 만들기
 	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
 	//--- 세이더 코드를 세이더 객체에 넣기
@@ -914,4 +1001,111 @@ int loadObj_normalize_center(const char* filename)
 	}
 
 	return outvertex.size();
+}
+int loadObj_normalize_center1(const char* filename)
+{
+	FILE* objFile;
+
+	fopen_s(&objFile, filename, "rb");
+
+	if (objFile == NULL) {
+		printf("Impossible to open the file !\n");
+		return false;
+	}
+	while (1) {
+
+		char lineHeader[128];
+		// read the first word of the line
+		int res = fscanf(objFile, "%s", lineHeader);
+		if (res == EOF)
+			break; // EOF = End Of File. Quit the loop.
+		if (strcmp(lineHeader, "v") == 0) {
+			glm::vec3 vertex;
+			fscanf(objFile, "%f %f %f\n", &vertex.x, &vertex.y, &vertex.z);
+
+			if (vertex.x < minX1) minX1 = vertex.x;
+			if (vertex.y < minY1) minY1 = vertex.y;
+			if (vertex.z < minZ1) minZ1 = vertex.z;
+			if (vertex.x > maxX1) maxX1 = vertex.x;
+			if (vertex.y > maxY1) maxY1 = vertex.y;
+			if (vertex.z > maxZ1) maxZ1 = vertex.z;
+			sumX1 += vertex.x;
+			sumY1 += vertex.y;
+			sumZ1 += vertex.z;
+
+			temp_vertices1.push_back(vertex);
+		}
+		else if (strcmp(lineHeader, "vt") == 0) {
+			glm::vec2 uv;
+			fscanf(objFile, "%f %f\n", &uv.x, &uv.y);
+			temp_uvs1.push_back(uv);
+		}
+		else if (strcmp(lineHeader, "vn") == 0) {
+			glm::vec3 normal;
+			fscanf(objFile, "%f %f %f\n", &normal.x, &normal.y, &normal.z);
+			temp_normals1.push_back(normal);
+		}
+		else if (strcmp(lineHeader, "f") == 0) {
+			std::string vertex1, vertex2, vertex3;
+			unsigned int vertexIndex[3], uvIndex[3], normalIndex[3];
+			int matches = fscanf(objFile, "%d/%d/%d %d/%d/%d %d/%d/%d\n", &vertexIndex[0], &uvIndex[0], &normalIndex[0], &vertexIndex[1], &uvIndex[1], &normalIndex[1], &vertexIndex[2], &uvIndex[2], &normalIndex[2]);
+			if (matches != 9) {
+				printf("File can't be read by our simple parser : ( Try exporting with other options\n");
+				return false;
+			}
+			vertexIndices1.push_back(vertexIndex[0]);
+			vertexIndices1.push_back(vertexIndex[1]);
+			vertexIndices1.push_back(vertexIndex[2]);
+			uvIndices1.push_back(uvIndex[0]);
+			uvIndices1.push_back(uvIndex[1]);
+			uvIndices1.push_back(uvIndex[2]);
+			normalIndices1.push_back(normalIndex[0]);
+			normalIndices1.push_back(normalIndex[1]);
+			normalIndices1.push_back(normalIndex[2]);
+		}
+	}
+
+	std::cout << "minX: " << minX << " minY: " << minY << " minZ: " << minZ << std::endl;
+	std::cout << "maxX: " << maxX << " maxY: " << maxY << " maxZ: " << maxZ << std::endl;
+
+	aveX1 = sumX1 / vertexIndices1.size();
+	aveY1 = sumY1 / vertexIndices1.size();
+	aveZ1 = sumZ1 / vertexIndices1.size();
+	scaleX1 = maxX1 - minX1;
+	scaleY1 = maxY1 - minY1;
+	scaleZ1 = maxZ1 - minZ1;
+
+	glm::vec3 temp;
+
+	std::cout << "aveX: " << aveX1 << " aveY: " << aveY1 << " aveZ: " << aveZ1 << std::endl;
+
+	std::cout << "scaleX: " << scaleX1 << " scaleY: " << scaleY1 << " scaleZ: " << scaleZ1 << std::endl;
+
+	for (unsigned int i = 0; i < vertexIndices1.size(); i++) {
+		unsigned int vertexIndex = vertexIndices1[i];
+		temp = temp_vertices1[vertexIndex - 1];
+		temp.x = temp.x - minX1;
+		temp.y = temp.y - minY1;
+		temp.z = temp.z - minZ1;
+
+		temp.x = ((temp.x * 2.0f) / scaleX1) - 1.0f;
+		temp.y = ((temp.y * 2.0f) / scaleY1) - 1.0f;
+		temp.z = ((temp.z * 2.0f) / scaleZ1) - 1.0f;
+
+		outvertex1.push_back(temp);
+		//glm::vec3 vertex = temp_vertices[vertexIndex - 1];
+		//outvertex.push_back(vertex);
+	}
+	for (unsigned int i = 0; i < uvIndices1.size(); i++) {
+		unsigned int uvIndex = uvIndices1[i];
+		glm::vec2 vertex = temp_uvs1[uvIndex - 1];
+		outuv1.push_back(vertex);
+	}
+	for (unsigned int i = 0; i < normalIndices1.size(); i++) {
+		unsigned int normalIndex = normalIndices1[i];
+		glm::vec3 vertex = temp_normals1[normalIndex - 1];
+		outnormal1.push_back(vertex);
+	}
+
+	return outvertex1.size();
 }
